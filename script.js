eaf3419173b046526debdb26c9094e9548579cfb
Kaspiper <Kasper.c.lau@gmail.com>
2024-06-29 11:52:51 -0700

    Website update
    Added some personal aesthetic design changes
    
    Added some webpage content

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+
 function toggleMenu(){
     const menu = document.querySelector(".menu-links");
     const icon = document.querySelector(".hamburger-icon");
+    const tstbtn = document.querySelector(".TESTBTN");
     menu.classList.toggle("open");
     icon.classList.toggle("open");
+    tstbtn.classList.toggle("open");
 }
+
+
+window.addEventListener("scroll", (event) => {
+    let scroll = this.scrollY;
+    if (scroll > 5) {
+      document.querySelector(".header-container").classList.add("shadow");
+    } else {
+      document.querySelector(".header-container").classList.remove("shadow");
+    }
+});
+
+
+
